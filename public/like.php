cdc4cae5d59c337b0018f849b6ce59483bc5604e
Dominiek ter Heide <dominiek@cerego.com>
2010-11-01 16:26:16 +0100

    Example like event catcher.

--- public/like.php
+++ public/like.php
@@ -1,10 +1,18 @@
 <?php require_once("../application.php"); ?>
 <html>
   <head>
-    <title>Anymeta Login</title>
+    <title>Anymeta Brick</title>
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js"></script>
     <script type="text/javascript" src="jquery.json.min.js"></script>
     <script type="text/javascript" src="application.js"></script>
+    <meta property="og:title" content="Synaptify Synapse"/>
+    <meta property="og:type" content="product"/>
+    <meta property="og:url" content="http://anymeta.synaptify.com/like.php"/>
+    <meta property="og:image" content="http://www.georgiapainphysicians.com/downloads/m1_slides/8.%20Synaptic%20cleft.jpg"/>
+    <meta property="og:site_name" content="Synaptify"/>
+    <meta property="fb:admins" content="<?= $FACEBOOK_APPLICATION_ID ?>"/>
+    <meta property="og:description"
+          content="This is an original Synaptify Synapse object"/>
   </head>
 
   <body>
